eda1603c9cd07fa19e41130ada666666463f5c0e
tikkaalikk <zbright8@gmail.com>
2015-12-01 00:39:24 -0400

    asdf

--- src/main/java/ca/unbsj/cs2043/rebelsimulator/Government.java
+++ src/main/java/ca/unbsj/cs2043/rebelsimulator/Government.java
@@ -12,12 +12,10 @@ package ca.unbsj.cs2043.rebelsimulator;
 public class Government {
 	
 	Ideology ideo;
-	String name;
 	double cohesion, orgSupport;
 	
 	public Government(Ideology i, String n, double c, double s) {
 		ideo = i;
-		name = n;
 		cohesion = c;
 		orgSupport = s;
 	}
@@ -25,20 +23,12 @@ public class Government {
 	public Government(int id, String un, String n, 
 			double a, double r, double m, double t, double c, double s) {
 		ideo = new Ideology(id, un, a, r, m, t);
-		name = n;
 		cohesion = c;
 		orgSupport = s;
 	}
 	
 	public void suppress() {
 		
-	}
-	
-	
-	@Override
-	public String toString() {
-		String q = name + ", " + cohesion + ", " + orgSupport + ", " + ideo.name;
-		return q;
 	}
 	
 	
--- src/main/java/ca/unbsj/cs2043/rebelsimulator/Population.java
+++ src/main/java/ca/unbsj/cs2043/rebelsimulator/Population.java
@@ -12,7 +12,6 @@ package ca.unbsj.cs2043.rebelsimulator;
 public class Population {
 	
 	Ideology ideo;
-	String name;
 	double baseRevolt, revolt, richPoor, govSupport, orgSupport, suppression;
 	boolean inRevolt = false;
 	boolean isSuppressed = false;
@@ -20,7 +19,6 @@ public class Population {
 	public Population(String n, double br, double rp, double su, double gs, 
 			int id, String un, double auth, double right, double trad, double mili) {
 		ideo = new Ideology(id, un, auth, right, mili, trad);
-		name = n;
 		baseRevolt = br;
 		richPoor = rp;
 		suppression = su;
@@ -28,9 +26,8 @@ public class Population {
 		//calc();
 	}
 	
-	public Population(Ideology i, String n, double br, double rp, double su, double gs) {
+	public Population(Ideology i, double br, double rp, double su, double gs) {
 		ideo = i;
-		name = n;
 		baseRevolt = br;
 		richPoor = rp;
 		suppression = su;
